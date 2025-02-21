7e1b6454a121939b687efc244c15a774141c19e5
Augusto <augustovmagro@gmail.com>
2025-02-21 00:23:15 -0300

    For, while and do while learning

---
+++ labtwo/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String fruit [] = {"Banada", "Apple", "Tomato", "Pinepple"};
+
+        for (int x = 0; x < fruit.length; x++){
+            System.out.println("Fruit: " + fruit[x]);
+        }
+    }
+}
