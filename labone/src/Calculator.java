1e07d1026a61ee009a96bc543bd4e6648de697fa
Augusto <augustovmagro@gmail.com>
2025-02-20 21:38:49 -0300

    Commiting lessons lerned as a documatation

---
+++ labone/src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public int sum(int n1, int n2){
+        int result = n1 + n2;
+        return result;
+    }
+    public int times(int n1, int n2){
+        int result = n1 * n2;
+        return result;
+    }
+}
--- labone/src/LabOne.java
+++ labone/src/LabOne.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class LabOne {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        Calculator calculator = new Calculator();
+
+        int number = 10;
+        int secondNumber = 11; 
+        
+        System.out.println(number);
+
+        // If and Else Abreviation (Third operator)
+        String result = number==secondNumber ? "True" : "False";
+        System.out.println(result);
+
+        // Getting input from users
+        System.out.println("Type the first number");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Type the second number");
+        int number2 = scanner.nextInt();
+
+        // Using functions
+        System.out.println(calculator.sum(number1, number2));
+        System.out.println(calculator.times(number1, number2));
+        System.out.println(number1 + "10");
+
     }
 }
